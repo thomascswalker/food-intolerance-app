37afaad85bdce1dd1e7ad4503628cad9b293a7af
thomascswalker <thomascswalker@users.noreply.github.com>
2026-02-20 19:18:39 -0500

    add search to table

--- My App.swiftpm/ContentView.swift
+++ My App.swiftpm/ContentView.swift
@@ -3,26 +3,36 @@ import Foundation
 
 struct ContentView: View {
     @State private var foods = [FoodItem]()
+    @State private var searchText: String = ""
     
     var body: some View {
-        VStack {
-            Text("Foods").fontWeight(.bold)
-            Spacer(minLength: 20)
-            Table(foods) {
-                TableColumn("Name", value: \.description)
-                TableColumn("Nutrients") { food in
-                    GroupBox {
-                        ForEach(food.nutrients) { nutrient in
-                            Text(nutrient.name + ", " + nutrient.amount.description + nutrient.unitName)
-                        }  
-                    }
-                }    
+            NavigationStack {
+                Table(searchResults) {
+                    TableColumn("Name", value: \.description)
+                    TableColumn("Nutrients") { food in
+                        GroupBox {
+                            ForEach(food.nutrients) { nutrient in
+                                Text(nutrient.name + ", " + nutrient.amount.description + nutrient.unitName)
+                            }  
+                        }
+                    }    
+                }
+                .navigationTitle("Foods")
+                .tableColumnHeaders(.visible)
+                .environment(\.defaultMinListRowHeight, 80) // Set your desired minimum height
             }
-            .tableColumnHeaders(.visible)
-            .environment(\.defaultMinListRowHeight, 80) // Set your desired minimum height
-        }.onAppear {
-            if let newData: [FoodItem] = loadJson(filename: "USDAFoods") {
-                foods = newData
+            .searchable(text: $searchText)
+            .onAppear {
+                if let newData: [FoodItem] = loadJson(filename: "USDAFoods") {
+                    foods = newData
+                }
+            }
+        
+        var searchResults: [FoodItem] {
+            if searchText.isEmpty {
+                return foods
+            } else {
+                return foods.filter{$0.description.contains(searchText)}
             }
         }
     }
